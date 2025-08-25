aea6d4a15e58dd84f0f48319ce83f8afaf033234
pakro <893843891@qq.com>
2025-08-25 23:08:46 +0800

    better error handler

--- web/src/pages/SubscriptionPage.js
+++ web/src/pages/SubscriptionPage.js
@@ -180,6 +180,7 @@ const SubscriptionPage = () => {
     try {
       const values = await form.validateFields();
       setLoading(true);
+      let data
 
       // 处理表单提交
       if (values.upload_type === 'file' && values.content) {
@@ -192,12 +193,16 @@ const SubscriptionPage = () => {
         const contentBlob = new Blob([values.content], {type: 'text/plain'});
         formData.append('file', contentBlob, 'subscription.txt');
 
-        await subscriptionApi.createProxyWithFormData(formData);
+        data = await subscriptionApi.createProxyWithFormData(formData);
       } else {
         // 处理URL提交
-        await subscriptionApi.createProxy({
+        data = await subscriptionApi.createProxy({
           type: values.type, upload_type: values.upload_type, url: values.url || '', content: values.content || ''
         });
+      }
+      if (data.result === 'fail') {
+        message.error(`添加订阅失败: ${data?.status_msg || '未知错误'}`);
+        return;
       }
 
       message.success('添加订阅成功');
